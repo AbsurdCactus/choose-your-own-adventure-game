ec77668fa569e53c7083c17fc6912b9000ac9af6
AbsurdCactus <braydonreber@gmail.com>
2021-10-23 17:30:18 -0400

    cleaned functions, learning how to test input

--- main.py
+++ main.py
@@ -1,60 +1,62 @@
 from ascii_art import intro_art
 
-print(intro_art)
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.")
-the_string = "hello world"
+def gooby():
+    print("hello world")
+def get_input():
+    return input()
 
 
-def starting_up():
-    # print(the_string)
-    return the_string
+def start():
+
+    print(intro_art)
+    print("Welcome to Treasure Island.")
+    print("Your mission is to find the treasure.")
+    first_step()
 
 
-def start_game():
+def first_step():
 
-    first_choice = input("Choose L for left, R for right: ")
+    print("Type \"left\" to go left, \"right\" to go right: ")
+    first_choice = get_input()
 
     if first_choice == "L":
-
         print("you go left")
+        second_step()
 
     else:
-
         print("You fall into a hole. Game over")
-        exit
 
-    second_choice = input("Choose S for Swim, W for wait: ")
+
+def second_step():
+
+    print("Choose S for swim, W for wait: ")
+
+    second_choice = get_input()
 
     if second_choice == "W":
-
         print("You wait")
+        third_step()
 
     else:
-
         print("Attacked by trout. Game over")
-        exit
 
-    third_choice = input("Choose R for Red, B for blue, Y for yellow: ")
+
+def third_step():
+
+    print("Choose R for Red, B for blue, Y for yellow: ")
+    third_choice = get_input()
 
     if third_choice == "Y":
-
         print("You Win!")
-        exit
 
     elif third_choice == "R":
-
         print("Burned by fire. Game Over.")
-        exit
 
     elif third_choice == "B":
         print("Eaten by beasts. Game over")
-        exit
 
     else:
-
         print("Game Over")
-        exit
 
 
-start_game()
+start()
--- test.py
+++ test.py
@@ -1,17 +1,29 @@
 import unittest
 import main
+from unittest.mock import patch
+from io import StringIO
+
 
 class TestMain(unittest.TestCase):
 
-    def test_hello_world(self):
-        
-        self.assertEqual(main.the_string, ("hello world"))
+    # incomplete
+    # this proves I can use a mock input
+    def test_first_step(self):
 
-    def test_starting_up(self):
-        self.assertEqual(main.starting_up(), "hello world")
+        string_of_input = "L"
 
-    def test_first_choice(self):
-        self.assertEqual(main.first_choice("L"), "left")
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            main.first_step()
+            self.assertEqual(fake_out.getvalue(
+            ), "Type \"left\" to go left, \"right\" to go right: \nYou fall into a hole. Game over\n")
+
+    def test_gooby(self):
+
+        # complete
+        # this proves I can test printed values
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            main.gooby()
+            self.assertEqual(fake_out.getvalue(), "hello world\n")
 
 
 if __name__ == '__main__':
